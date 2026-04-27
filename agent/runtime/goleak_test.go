3ee978d7490d736fa316adebd8f9af140add3aa5
BaSui <basui@agentflow.dev>
2026-04-28 02:44:59 +0800

    test: 更新 goleak 测试配置

--- agent/runtime/goleak_test.go
+++ agent/runtime/goleak_test.go
@@ -9,5 +9,8 @@ import (
 func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m,
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
+		goleak.IgnoreAnyFunction("github.com/BaSui01/agentflow/agent/runtime.(*ManagedLSP).start"),
+		goleak.IgnoreTopFunction("io.(*pipe).read"),
+		goleak.IgnoreTopFunction("io.(*pipe).write"),
 	)
 }
